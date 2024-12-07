888a64f994ab734b28ab6a7935f6a9d4ff27aca1
SohamD34 <deshmukh.2@iitj.ac.in>
2024-12-08 00:43:10 +0530

    Added: modules, app instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { StagesModule } from './stages/stages.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
+import { CommentsModule } from './comments/comments.module';
+import { LikesModule } from './likes/likes.module';
+import { RepostsModule } from './reposts/reposts.module';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [AuthModule, StagesModule, NotificationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),  // If you are using environment variables
+    MongooseModule.forRoot('mongodb+srv://SohamD:Soham34@cluster0.wl8as.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),  // MongoDB connection URI
+    StagesModule, 
+    CommentsModule, LikesModule, RepostsModule, NotificationsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Authentication API')
+    .setDescription('API documentation for user authentication')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
